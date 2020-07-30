3d3543f08a80fe3b97e8e1cdc430d01d907d4057
William Lockwood <jinkside@gmail.com>
2020-07-30 14:43:00 -0700

    Adding test-scripts to repo

---
+++ test-scripts/cv-video-test.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2 as cv
+from copy import deepcopy
+from time import perf_counter as pc
+
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+	print("Cannot open camera")
+	exit()
+while True:
+	# Capture frame-by-frame
+	ret, frame = cap.read()
+	
+	# if frame is read correctly ret is True
+	if not ret:
+		print("Can't receive frame (stream end?) Exiting...")
+		break
+	
+	# Our operations on the frame come here
+	edges = cv.Canny(frame, 100, 200, 3)
+
+	# Display the resulting frame
+	cv.imshow('frame', edges)
+	
+	if cv.waitKey(1) == ord('q'):
+		break
+	
+# When done, release the camera
+cap.release()
+cv.destroyAllWindows()
